2853b43bdfbbec2ae49da9769719c0eef752ba65
nikek <nikek@spotify.com>
2019-10-30 21:22:27 +0100

    update packages, dark mode, remove storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
 
 function loadStories() {
   require('../src/stories');
 }
+
+addParameters({
+  options: {
+    theme: themes.dark
+  }
+});
 
 configure(loadStories, module);
